362e3b5b1f91680e9ffda2bbaabdf91f02fd2329
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-04-14 16:19:19 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vm_name" {
-  value = "${vsphere_virtual_machine.main.name}"
+  value = vsphere_virtual_machine.main.name
 }
 
 output "vm_management_ip" {
-  value = "${var.management_ipv4_address}"
+  value = var.management_ipv4_address
 }
